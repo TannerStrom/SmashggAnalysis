7e44c08deced2cb4402a0a52ef9d3021f71bfedf
Tanner Strom <tanner.strom@gmail.com>
2016-12-23 20:43:59 -0500

    am able to get a player

--- js/tournament.js
+++ js/tournament.js
@@ -1,17 +1,44 @@
-//function getName() {
-  var url = 'https://api.smash.gg/tournament/bopme-15/attendees';
+$.ajax({
+  url: 'https://api.smash.gg/tournament/bopme-15',
+  type: 'GET',
+  dataType: 'json',
+  success: setTitle
+})
+
+$.ajax({
+  url: 'https://api.smash.gg/event/14870?expand[0]=entrants&expand[1]=groups',
+  type: 'GET',
+  dataType: 'json',
+  success:getEntrantID
+})
+
+function setTitle(data) {
+  var name = data['entities']['tournament']['name'];
+  $('.tournament-name').text(name);
+}
+
+function getEntrantID(data) {
+  var entrants = data['entities']['entrants'];
   
   $.ajax({
-    url: url,
+    url: 'https://api.smash.gg/entrant/326304',
     type: 'GET',
     dataType: 'json',
-    success: displayAll
+    success: getPlayer
   })
-  
-  //$('.tournament-name').text("Tournament Name");
-//}
+}
 
-function displayAll(data) {
-  var response = JSON.parse(data);
-  console.log(response);
+function getPlayer(data) {
+  var id = data['entities']['attendee'][0]['playerId'];
+  
+  $.ajax({
+    url: 'https://api.smash.gg/player/' + id,
+    type: 'GET',
+    dataType: 'json',
+    success: gotPlayer
+  })
+}
+
+function gotPlayer(data) {
+  console.log(data);
 }
