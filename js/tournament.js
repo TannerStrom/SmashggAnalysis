caf10cdf9b0632845ee0d5c5dbc872295884091e
Tanner Strom <tstrom@purdue.edu>
2016-12-23 23:32:38 -0500

    populates a graph with player data from bopme 15, js is ugly and has to be redone

--- js/tournament.js
+++ js/tournament.js
@@ -44,5 +44,46 @@ function getPlayer(data) {
 }
 
 function gotPlayer(data) {
-  console.log(data['entities']['player']['gamerTag']);
+  var state = data['entities']['player']['state'];
+  prepData(state);
+}
+
+var IN = 0;
+  var KY = 0;
+  var IL = 0;
+  var other = 0;
+
+function prepData(state) {
+  google.charts.load('current', {'packages':['corechart', 'bar']});
+  google.charts.setOnLoadCallback(makeChart);
+  
+  
+  
+  if(state === "IN") {
+    IN++;
+  } else if (state === "IL") {
+    IL++;
+  } else if (state === "KY") {
+    KY++;
+  } else {
+    other++;
+  }
+  
+  function makeChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'State');
+    data.addColumn('number', 'Total');
+    data.addRows([
+      ['IN', IN],
+      ['KY', KY],
+      ['IL', IL],
+      ['Other', other]
+    ]);
+    
+    var options = {'title': "Players per State",
+                   'width': 500,
+                   'height': 400};
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
 }
